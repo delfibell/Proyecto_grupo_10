96fe4430eb556f6a2672d27579a88a9d061d8cd8
Koichi Kairiyama <koichi.kairiyama@gmail.com>
2022-09-21 12:17:36 -0300

    Creación de carpetas .views, public,etc

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
-/* app.get('/', (req,res)=>{
+ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '');
-});    */ 
+});   
