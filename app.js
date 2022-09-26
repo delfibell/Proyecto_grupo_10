ea68304d75aa1333f148d4f55f0218de3dea5006
Fermin Gutierrez añeli <fermingutierrezaneli@MacBook-Pro-de-Fermin.local>
2022-09-25 22:34:38 -0300

    Detalle de producto mobile main

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
- app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-});   
+// app.get('/', (req,res)=>{
+//    res.sendFile(path.join(__dirname, '/views/index.html'));
+//})   
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/detalle-de-producto.html'));
+});  
+
+
