d86ae01e45c712c20bd252430a65a21a13b4b025
julibertolini <bertolini.julieta@gmail.com>
2022-10-04 14:50:49 -0300

    cambios en css carrito

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ app.get('/detalle-de-producto', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/detalle-de-producto.html'));
 } );
 
+//borrar
 app.get('/headerfooter', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/Header&Footer.html'));
 } );
 
+//cambiar a registro
 app.get('/formulario-de-registro', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/formularioDeRegistro.html'));
 } );
 
+//cambiar a login
 app.get('/formularioDeRegistro.html', (req,res)=>{
     res.sendFile(path.join(__dirname, '/views/formularioDeRegistro.html'));
 } );
