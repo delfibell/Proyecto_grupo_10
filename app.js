1a8ae332accb39005785c37a8f1642bd6727822c
Fermin Gutierrez añeli <fermingutierrezaneli@MacBook-Pro-de-Fermin.local>
2022-10-15 13:08:03 -0300

    pusheo revision con rocio rutas y controllers

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+const mainRoutes = require("./src/routes/main");
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-}); 
+app.listen(3000, () => {
+  console.log("Servidor funcionando");
+});
 
-app.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-} );
+app.use("/", mainRoutes);
 
-app.get('/detalle-de-producto', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/detalle-de-producto.html'));
-} );
+app.get("/detalle-de-producto", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/src/views/products/detalle-de-producto.html")
+  );
+});
 
-app.get('/registro', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/formularioDeRegistro.html'));
-} );
+app.get("/registro", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "/src/views/users/formularioDeRegistro.html")
+  );
+});
 
-app.get('/login', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/formularioDeLogin.html'));
-} );
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/views/users/formularioDeLogin.html"));
+});
 
-app.get('/carrito', (req,res)=>{
-    res.sendFile(path.join(__dirname, '/views/carrito.html'));
-} );
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname, "/src/views/products/carrito.html"));
+});
