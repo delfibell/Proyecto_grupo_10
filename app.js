7915c0eaa88c235101f75d6b1734e368796b79a0
David Castellar Martinez <castellardav@gmail.com>
2022-10-18 16:41:48 -0500

    selecciona el motor de templates y la ruta de las views

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const notFoundRoutes = require("./src/routes/notFound")
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "src/views"))
 
 app.listen(3000, () => {
   console.log("Servidor funcionando");
