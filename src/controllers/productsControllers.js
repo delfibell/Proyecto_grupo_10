9f06cf04edfb007b7a0349de9a7d56833f62c775
julibertolini <bertolini.julieta@gmail.com>
2022-10-20 23:27:02 -0300

    pÃaginas de creacion y edicion de producto (en ejs (sin css propio), nuevas rutas y controladores para nuevas paginas, correccion en ejs login y registro

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -4,6 +4,12 @@ let productsControllers = {
   detalleProducto: (req, res) => {
     res.render("products/detalleDeProducto")
   },
+  crearProducto: (req, res) => {
+    res.render("products/creacionProducto")
+  },
+  editarProducto: (req, res) => {
+    res.render("products/edicionProducto")
+  },
 };
 
 module.exports = productsControllers;
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,5 +3,7 @@ const productsControllers = require("../controllers/productsControllers.js");
 const router = express.Router();
 
 router.get("/detalle-de-producto", productsControllers.detalleProducto);
+router.get("/crear", productsControllers.crearProducto);
+router.get("/editar", productsControllers.editarProducto)
  
 module.exports = router;
