3049cca81acb3a6252378c659bec3ca6af52e9f6
julibertolini <bertolini.julieta@gmail.com>
2022-10-20 23:33:14 -0300

    cambio nombre ruta detalle de producto

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const productsControllers = require("../controllers/productsControllers.js");
 const router = express.Router();
 
-router.get("/detalle-de-producto", productsControllers.detalleProducto);
+router.get("/detalle", productsControllers.detalleProducto);
 router.get("/crear", productsControllers.crearProducto);
 router.get("/editar", productsControllers.editarProducto)
  
