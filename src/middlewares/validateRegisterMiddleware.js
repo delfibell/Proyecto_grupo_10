dd73fbf5fd6bf86ba45a6902d0b421c859fca6b7
julibertolini <bertolini.julieta@gmail.com>
2023-01-03 22:02:54 -0300

    cambios en el footer

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -2,18 +2,19 @@ const path = require('path');
 const { body } = require('express-validator');
 
 const validateCreateForm = [
-	body('fullName').notEmpty().withMessage('Tienes que escribir un nombre'),
+	body('firstName').notEmpty().withMessage('Tenés que escribir un nombre'),
+	body('lastName').notEmpty().withMessage('Tenés que escribir un apellido'),
 	body('email')
-		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
-		.isEmail().withMessage('Debes escribir un formato de correo válido'),
-	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-	body('country').notEmpty().withMessage('Tienes que elegir un país'),
-	body('avatar').custom((value, { req }) => {
+		.notEmpty().withMessage('Tenés que escribir un correo electrónico').bail()
+		.isEmail().withMessage('Tenés que escribir un formato de correo válido'),
+	body('password').notEmpty().withMessage('Tenés que escribir una contraseña'),
+	body('username').notEmpty().withMessage('Tenés que escribir un nombre de usuario'),
+	body('profilePic').custom((value, { req }) => {
 		let file = req.file;
 		let acceptedExtensions = ['.jpg', '.png', '.gif'];
 
 		if (!file) {
-			throw new Error('Tienes que subir una imagen');
+			throw new Error('Tenés que subir una imagen');
 		} else {
 			let fileExtension = path.extname(file.originalname);
 			if (!acceptedExtensions.includes(fileExtension)) {
